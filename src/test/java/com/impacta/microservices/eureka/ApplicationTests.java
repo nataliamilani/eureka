fecc368d1293097501b9ca045e72a39f59937364
Natalia Milani <natalia.milani@hotmail.com>
2021-09-24 01:49:30 -0300

    ajustando estrutura

---
+++ src/test/java/com/impacta/microservices/eureka/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.impacta.microservices.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
